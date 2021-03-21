4f07da95b3b5de361efbed0b858bca92458c9bfa
Alyssa Holland <alysam0071@gmail.com>
2021-03-21 15:48:38 -0400

    Display tooltip when copying is successful

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 const copyBtn = document.getElementById("copy-btn");
 
-const copyCode = () => {
+const copyCSS = () => {
   const codeEl = document.getElementsByTagName("code")[0];
-  console.log("CopyCode: ", codeEl);
+
   window.navigator.clipboard
     .writeText(codeEl.innerText.trim())
     .then(() => {
-      //TODO: Display tooltip indicating content was copied
-      console.log("Copied to clipboard");
+        copyBtn.classList.toggle("tooltip") //Toggle tooltip display on
+        setTimeout(() => {
+            copyBtn.classList.toggle("tooltip") //After 1 sec, toggle tooltip display off
+        }, 1000)
     })
     .catch(() => console.error("Error: Unable to copy to clipboard"));
 };
 
-copyBtn.addEventListener("click", copyCode);
+copyBtn.addEventListener("click", copyCSS);
